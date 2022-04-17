a2a47a946ab3afba9c70614273e954ac277ccfe7
therobite <antonielcamposm@gmail.com>
2022-04-17 01:56:17 -0300

    Create: Criado arquivos iniciais

---
+++ api.js
@@ -0,0 +1,26 @@
+let hoje = new Date()
+const mesAtual = String(hoje.getMonth() + 1).padStart(2, '0')
+let dataAtual = hoje.getFullYear()+'-'+mesAtual+'-'+hoje.getDate()
+const api_url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dataAtual}&end_date=${dataAtual}&api_key=BRxEfpRHnuPrToZ9c4BdPpKynsKUrlcUuGVfAHge`
+
+
+
+let totalAsteroides = 0
+const listaAsteroides = []
+
+
+async function obterAsteroides(){
+
+    const response = await fetch(api_url)
+    const data = await response.json()
+
+    totalAsteroides = data.element_count
+    listaAsteroides.push({...data.near_earth_objects})
+   
+}
+obterAsteroides()
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,78 @@
+const divContainerAsteroides = document.getElementById("container-cards")
+const divTotalAsteroides = document.getElementById("total-asteroides")
+
+
+
+
+function filtrarSomenteAsteroides(){
+
+    const asteroides = listaAsteroides.map((elem)=>{
+      return elem[dataAtual]
+    
+    })
+  
+   return asteroides[0]
+}
+
+
+
+function criarTextoTotal(){
+    setTimeout(()=>{
+        const h1 = document.createElement("h1")
+        h1.innerText = `Total de Asteróides de hoje: ${totalAsteroides}` 
+        divTotalAsteroides.append(h1)
+    }, 500)
+         
+
+}
+criarTextoTotal()
+
+
+function criarCard(){
+    divContainerAsteroides.innerHTML=""
+   
+    setTimeout(()=>{
+        const asteroides = filtrarSomenteAsteroides()
+        asteroides.forEach((elem)=>{
+            
+            console.log(elem)
+
+            const divCard = document.createElement("div")
+            const velocidade = elem.close_approach_data[0].relative_velocity.kilometers_per_second
+            const aproximacao = elem.close_approach_data[0].close_approach_date
+            const aproximacaoFull = elem.close_approach_data[0].close_approach_date_full
+
+            let nomeSemParenteses= elem.name.replaceAll("(", "").replaceAll(")", "")
+            let aproximacaoReverse = aproximacao.split("-").reverse().join().replaceAll(",", "-")
+            let aproximacaoHoras =  aproximacaoFull.slice(-5)   
+            let converterParaInteiro = parseInt(velocidade)
+            
+            divCard.setAttribute("class", "card")
+
+            if(elem.is_potentially_hazardous_asteroid == false){
+                divCard.innerHTML=
+                `
+                <h4 class="nome-asteroide">Nome: ${nomeSemParenteses}</h4>
+                <h4 class="nome-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
+                <h4 class="nome-asteroide">Aproximação em ${aproximacaoReverse} ás ${ aproximacaoHoras}</h4>
+                <h4 class="asteroide-nao-perigoso ">Não é um asteróide potencialmente perigoso</h4>
+                `
+                divContainerAsteroides.append(divCard)
+            } else {
+                divCard.innerHTML=
+                `
+                <h4 class="nome-asteroide">Nome: ${nomeSemParenteses}</h4>
+                <h4 class="nome-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
+                <h4 class="nome-asteroide">Aproximação em ${aproximacaoReverse} ás ${ aproximacaoHoras}</h4>
+                <h4 class="asteroide-perigoso">É um asteróide potencialmente perigoso</h4>
+                `
+                divContainerAsteroides.append(divCard)
+            }
+
+           
+        })
+    }, 500)
+    
+ 
+}
+criarCard()
