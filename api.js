fab07453b55722004ef382806ce0e6b16370337a
therobite <antonielcamposm@gmail.com>
2022-04-26 10:38:17 -0300

    Update: reajustado o tempo de resposta da API

--- api.js
+++ api.js
@@ -2,19 +2,36 @@ let hoje = new Date()
 const mesAtual = String(hoje.getMonth() + 1).padStart(2, '0')
 let dataAtual = hoje.getFullYear()+'-'+mesAtual+'-'+hoje.getDate()
 const api_url =  `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dataAtual}&end_date=${dataAtual}&api_key=BRxEfpRHnuPrToZ9c4BdPpKynsKUrlcUuGVfAHge`
-
+const btnPesquisar = document.getElementById("botao-pesquisar")
 
 
 
 let totalAsteroides = 0
 const listaAsteroides = []
+let dataUsuario = ""
 
+btnPesquisar.addEventListener("click", ()=>{
+    let inputParaString = input.value.toString()
+    dataUsuario = inputParaString
+    let apiUsuario =  `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dataUsuario}&end_date=${dataUsuario}&api_key=BRxEfpRHnuPrToZ9c4BdPpKynsKUrlcUuGVfAHge`
+    listaAsteroides.length = 0  
+    obterAsteroides(apiUsuario)
+    criarTexto(dataUsuario)
+    divTotalAsteroides.innerHTML=""
+    divContainerAsteroides.innerHTML=""  
+    const h2 = document.createElement("h2")
+    h2.innerText = `Buscando data` 
+    divContainerAsteroides.append(h2)
+   
+   })
 
 async function obterAsteroides(api=api_url){
     const response = await fetch(api)
     const data = await response.json()
-    totalAsteroides = data.element_count
+    totalAsteroides = data.element_count   
     listaAsteroides.push({...data.near_earth_objects})
+    criarCard(totalAsteroides, dataUsuario)
+    
    
 }
 obterAsteroides()
--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const divTotalAsteroides = document.getElementById("total-asteroides")
 const divContainerAsteroides = document.getElementById("container-cards")
 const divSelecionarData = document.getElementById("selecionar-data")
 const input = document.getElementById("input")
-const btnPesquisar = document.getElementById("botao-pesquisar")
 
 
-let dataUsuario = ""
+
+
 
 function imagemAleatoria(){
     return Math.floor(Math.random()*22)
@@ -20,163 +20,186 @@ function filtrarAsteroides(data=dataAtual){
     return asteroides[0]
 }
 
-function criarCard(){
+function criarCard(totalAsteroides, dataUsuario){
    
+    if(totalAsteroides > 0){      
+        if(dataUsuario === ""){
+            divTotalAsteroides.innerHTML=""
+            divContainerAsteroides.innerHTML=""
+            const h2 = document.createElement("h2")
+            h2.innerText = `Buscando data` 
+            divContainerAsteroides.append(h2)
+            setTimeout(()=>{
+            criarTexto()
     
-    if(dataUsuario === ""){
-        divTotalAsteroides.innerHTML=""
-        divContainerAsteroides.innerHTML=""
-        const h2 = document.createElement("h2")
-        h2.innerText = `Buscando data` 
-        divContainerAsteroides.append(h2)
-        setTimeout(()=>{
-        criarTexto()
-
-        divContainerAsteroides.innerHTML=""
-        const asteroides = filtrarAsteroides()
+            divContainerAsteroides.innerHTML=""
+            const asteroides = filtrarAsteroides()
+            
+            asteroides.sort(function(a,b){
+                    return new Date(a.close_approach_data[0].close_approach_date_full) - new Date(b.close_approach_data[0].close_approach_date_full)
+                    });
+            
+            asteroides.forEach((elem)=>{
+                const divCard = document.createElement("div")
+                const velocidade = elem.close_approach_data[0].relative_velocity.kilometers_per_second
+                const aproximacaoFull = elem.close_approach_data[0].close_approach_date_full
+                const diametroMinimo = elem.estimated_diameter.meters.estimated_diameter_min
+                const diametroMaximo = elem.estimated_diameter.meters.estimated_diameter_max
         
-        asteroides.sort(function(a,b){
-                return new Date(a.close_approach_data[0].close_approach_date_full) - new Date(b.close_approach_data[0].close_approach_date_full)
-                });
-        
-        asteroides.forEach((elem)=>{
-            const divCard = document.createElement("div")
-            const velocidade = elem.close_approach_data[0].relative_velocity.kilometers_per_second
-            const aproximacaoFull = elem.close_approach_data[0].close_approach_date_full
-            const diametroMinimo = elem.estimated_diameter.meters.estimated_diameter_min
-            const diametroMaximo = elem.estimated_diameter.meters.estimated_diameter_max
+                let nomeSemParenteses= elem.name.replaceAll("(", "").replaceAll(")", "")
+                let aproximacaoHoras =  aproximacaoFull.slice(-5)   
+                let converterParaInteiro = parseInt(velocidade)
+                
     
-            let nomeSemParenteses= elem.name.replaceAll("(", "").replaceAll(")", "")
-            let aproximacaoHoras =  aproximacaoFull.slice(-5)   
-            let converterParaInteiro = parseInt(velocidade)
-            
-
-            
-            divCard.setAttribute("class", "card")
-            divCard.style.backgroundImage=`url(./img/${imagemAleatoria()}.jpg)`
-            divCard.style.backgroundSize= "190%"
-            
-            
-            if(elem.is_potentially_hazardous_asteroid == false){
-                divCard.innerHTML=
-                `
-                <div class = "descricao-asteroide"> 
+                
+                divCard.setAttribute("class", "card")
+                divCard.style.backgroundImage=`url(./img/${imagemAleatoria()}.jpg)`
+                divCard.style.backgroundSize= "190%"
+                
+                
+                if(elem.is_potentially_hazardous_asteroid == false){
+                    divCard.innerHTML=
+                    `
+                    <div class = "descricao-asteroide"> 
+                        <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
+                        <h4 class="id-asteroide">ID: ${elem.id}</h4>
+                        <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
+                        <h4 class="aproximacao-asteroide">Hoje às ${aproximacaoHoras}</h4>
+                        <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed() } e ${diametroMaximo.toFixed()} metros de Diâmetro </h4>
+                    </div>
+                    <div class="div-asteroide-perigoso">
+                            <h4 class="asteroide-nao-perigoso ">Não é um Asteróide perigoso</h4>
+                    </div>    
+                    
+                    `
+                    divContainerAsteroides.append(divCard)
+                } else {
+                    divCard.innerHTML=
+                    `
+                    <div class = "descricao-asteroide"> 
                     <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
                     <h4 class="id-asteroide">ID: ${elem.id}</h4>
                     <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
                     <h4 class="aproximacao-asteroide">Hoje às ${aproximacaoHoras}</h4>
-                    <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed() } e ${diametroMaximo.toFixed()} metros de Diâmetro </h4>
-                </div>
-                <div class="div-asteroide-perigoso">
+                    <h4 class="diametro-asteroide">Entre ${diametroMinimo } e  ${diametroMaximo} metros de Diâmetro </h4>
+                    </div>
+                    
+                    <div class="div-asteroide-perigoso">
+                            <h4 class="asteroide-perigoso">É um Asteróide perigoso</h4>
+                    </div>   
+                    
+                    
+                    `
+                    divContainerAsteroides.append(divCard)
+                    
+                }
+                
+                
+            })
+            }, 1000)
+
+       
+
+
+
+
+        }
+        if(dataUsuario != "") {
+
+            divTotalAsteroides.innerHTML=""
+            divContainerAsteroides.innerHTML=""
+            const h2 = document.createElement("h2")
+            h2.innerText = `Buscando data` 
+            divContainerAsteroides.append(h2)
+            criarTexto(dataUsuario)
+
+
+            divContainerAsteroides.innerHTML=""  
+           
+            const asteroides =  filtrarAsteroides(dataUsuario)
+            console.log(asteroides)
+            asteroides.sort(function(a,b){
+                    return new Date(a.close_approach_data[0].close_approach_date_full) - new Date(b.close_approach_data[0].close_approach_date_full)
+                });
+            
+            asteroides.forEach((elem)=>{
+                
+
+                let DataReverse = dataUsuario.split("-").reverse().join().replaceAll(",", "-")
+
+                const divCard = document.createElement("div")
+                const velocidade = elem.close_approach_data[0].relative_velocity.kilometers_per_second
+                const aproximacaoFull = elem.close_approach_data[0].close_approach_date_full
+                const diametroMinimo = elem.estimated_diameter.meters.estimated_diameter_min
+                const diametroMaximo = elem.estimated_diameter.meters.estimated_diameter_max
+
+                let nomeSemParenteses= elem.name.replaceAll("(", "").replaceAll(")", "")
+                let aproximacaoHoras =  aproximacaoFull.slice(-5)   
+                let converterParaInteiro = parseInt(velocidade)
+                
+
+            
+                divCard.setAttribute("class", "card")
+                divCard.style.backgroundImage=`url(./img/${imagemAleatoria()}.jpg)`
+                divCard.style.backgroundSize= "190%"
+                
+                
+                if(elem.is_potentially_hazardous_asteroid == false){
+                    divCard.innerHTML=
+                    `
+                    <div class = "descricao-asteroide"> 
+                        <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
+                        <h4 class="id-asteroide">ID: ${elem.id}</h4>
+                        <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
+                        <h4 class="aproximacao-asteroide">No dia ${DataReverse} às  ${aproximacaoHoras}</h4>
+                        <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed()} e  ${diametroMaximo.toFixed()} metros de Diâmetro </h4>
+                    </div>
+                    <div class="div-asteroide-perigoso">
                         <h4 class="asteroide-nao-perigoso ">Não é um Asteróide perigoso</h4>
-                </div>    
+                    </div>    
+                    
+                    `
+                    divContainerAsteroides.append(divCard)
+                } else {
+                    divCard.innerHTML=
+                    `
+                    <div class = "descricao-asteroide"> 
+                        <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
+                        <h4 class="id-asteroide">ID: ${elem.id}</h4>
+                        <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
+                        <h4 class="aproximacao-asteroide">No dia ${DataReverse} às  ${aproximacaoHoras}</h4>
+                        <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed(0) } e  ${diametroMaximo.toFixed(0)} metros de Diâmetro </h4>
+                    </div>
                 
-                `
-                divContainerAsteroides.append(divCard)
-            } else {
-                divCard.innerHTML=
-                `
-                <div class = "descricao-asteroide"> 
-                <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
-                <h4 class="id-asteroide">ID: ${elem.id}</h4>
-                <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
-                <h4 class="aproximacao-asteroide">Hoje às ${aproximacaoHoras}</h4>
-                <h4 class="diametro-asteroide">Entre ${diametroMinimo } e  ${diametroMaximo} metros de Diâmetro </h4>
-                </div>
-                
-                <div class="div-asteroide-perigoso">
+                    <div class="div-asteroide-perigoso">
                         <h4 class="asteroide-perigoso">É um Asteróide perigoso</h4>
-                </div>   
+                    </div>   
                 
-                
-                `
-                divContainerAsteroides.append(divCard)
-                
-            }
-            
-            
-        })
-        }, 1000)
-    
-    } 
-    
-    if(dataUsuario != "") {
+                    
+                    `
+                    divContainerAsteroides.append(divCard)
+                }
 
-        divContainerAsteroides.innerHTML=""  
-        const asteroides =  filtrarAsteroides(dataUsuario)
-        asteroides.sort(function(a,b){
-                return new Date(a.close_approach_data[0].close_approach_date_full) - new Date(b.close_approach_data[0].close_approach_date_full)
-            });
-        
-        asteroides.forEach((elem)=>{
             
-
-            let DataReverse = dataUsuario.split("-").reverse().join().replaceAll(",", "-")
-
-            const divCard = document.createElement("div")
-            const velocidade = elem.close_approach_data[0].relative_velocity.kilometers_per_second
-            const aproximacaoFull = elem.close_approach_data[0].close_approach_date_full
-            const diametroMinimo = elem.estimated_diameter.meters.estimated_diameter_min
-            const diametroMaximo = elem.estimated_diameter.meters.estimated_diameter_max
-
-            let nomeSemParenteses= elem.name.replaceAll("(", "").replaceAll(")", "")
-            let aproximacaoHoras =  aproximacaoFull.slice(-5)   
-            let converterParaInteiro = parseInt(velocidade)
-            
+            })
 
         
-            divCard.setAttribute("class", "card")
-            divCard.style.backgroundImage=`url(./img/${imagemAleatoria()}.jpg)`
-            divCard.style.backgroundSize= "190%"
-            
-            
-            if(elem.is_potentially_hazardous_asteroid == false){
-                divCard.innerHTML=
-                `
-                <div class = "descricao-asteroide"> 
-                    <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
-                    <h4 class="id-asteroide">ID: ${elem.id}</h4>
-                    <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
-                    <h4 class="aproximacao-asteroide">No dia ${DataReverse} às  ${aproximacaoHoras}</h4>
-                    <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed()} e  ${diametroMaximo.toFixed()} metros de Diâmetro </h4>
-                </div>
-                <div class="div-asteroide-perigoso">
-                    <h4 class="asteroide-nao-perigoso ">Não é um Asteróide perigoso</h4>
-                </div>    
-                
-                `
-                divContainerAsteroides.append(divCard)
-            } else {
-                divCard.innerHTML=
-                `
-                <div class = "descricao-asteroide"> 
-                    <h4 class="nome-asteroide">${nomeSemParenteses}</h4>
-                    <h4 class="id-asteroide">ID: ${elem.id}</h4>
-                    <h4 class="velocidade-asteroide">Velocidade: ${converterParaInteiro} KM/s</h4>
-                    <h4 class="aproximacao-asteroide">No dia ${DataReverse} às  ${aproximacaoHoras}</h4>
-                    <h4 class="diametro-asteroide">Entre ${diametroMinimo.toFixed() } e  ${diametroMaximo.toFixed()} metros de Diâmetro </h4>
-                </div>
-            
-                <div class="div-asteroide-perigoso">
-                    <h4 class="asteroide-perigoso">É um Asteróide perigoso</h4>
-                </div>   
-            
-                
-                `
-                divContainerAsteroides.append(divCard)
-            }
+        }
 
-        
-        })
-        
-              
+      
+    }else {
+        divTotalAsteroides.innerHTML=""
+            divContainerAsteroides.innerHTML=""
+            const h2 = document.createElement("h2")
+            h2.innerText = `Nenhum Asteróide encontrado` 
+            divContainerAsteroides.append(h2)
     }
    
     
 }
-criarCard()
 
-function criarTexto(){
+
+function criarTexto(dataUsuario=""){
     divTotalAsteroides.innerHTML=""
     if(dataUsuario == ""){
         const h2 = document.createElement("h2")
@@ -193,7 +216,8 @@ function criarTexto(){
             divTotalAsteroides.append(h2)
               
            
-        } else {
+        } 
+        if(dataUsuario <  dataAtual) {
             divTotalAsteroides.innerHTML=""
             const h2 = document.createElement("h2")
             h2.innerText = `Passaram ${totalAsteroides} Asteróides` 
@@ -212,24 +236,7 @@ function criarTexto(){
 
 
 
-btnPesquisar.addEventListener("click", ()=>{
-    let inputParaString = input.value.toString()
-    dataUsuario = inputParaString
-    let apiUsuario =  `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dataUsuario}&end_date=${dataUsuario}&api_key=BRxEfpRHnuPrToZ9c4BdPpKynsKUrlcUuGVfAHge`
-    listaAsteroides.length = 0  
-    obterAsteroides(apiUsuario)
-    divTotalAsteroides.innerHTML=""
-    divContainerAsteroides.innerHTML=""  
-    const h2 = document.createElement("h2")
-    h2.innerText = `Buscando data` 
-    divContainerAsteroides.append(h2)
-   
-    setTimeout(()=>{
-        criarCard()
-        criarTexto()
-       
-    },1500) 
-   })
+
 
 
 
